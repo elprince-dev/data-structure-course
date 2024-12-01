8ec293904467328685f0043cfd1816e3eb3f99a6
Mohamed Hussein <mohamedmhussein@protonmail.com>
2024-12-01 00:08:06 -0500

    Updates the  bubbleSort

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,6 +1,20 @@
 function bubbleSort(arr){
+    let noSwaps = true;
+    for (let i = arr.length; i > 0; i--){
+        noSwaps = true
+        for (let n=0; n < i-1; n++){
+            console.log(arr[n], arr[n+1])
+            if (arr[n] > arr[n+1]){
+                [arr[n], arr[n+1]]=[arr[n+1],arr[n]]
+                noSwaps = false
+            }
+
+        }
+        if (noSwaps) break;
+        
+    }
 
     return arr
 }
 
-bubbleSort([23, 54, 243, 1, 76])
+console.log(bubbleSort([23, 54, 243, 1, 76, 0]))
