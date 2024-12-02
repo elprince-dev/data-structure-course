b7e3cf66216afc69e77e42e331756b0cc4422b23
Mohamed Hussein <mohamedmhussein@protonmail.com>
2024-12-02 00:18:34 -0500

    Updates the findEvenIndex file

--- findEvenIndex.js
+++ findEvenIndex.js
@@ -1,4 +1,18 @@
-const findEvenIndex = () => {
+function findEvenIndex(arr){
+
+
+    //[20,10,-80,10,10,15,35]
+    // 0   1   2  3  4  5  6
+    let sumLeft = 0;
+    let sumRight = arr.slice(1).reduce((num, accum) => num + accum, 0)
+    if(sumLeft === sumRight) return 0
+  
+    for (let i = 1; i < arr.length; i++){
+        sumLeft += arr[i-1]
+        sumRight-= arr[i]
+        if(sumLeft === sumRight) return i
+    }
+    return -1
   
 }
-
+findEvenIndex([20,10,-80,10,10,15,35])
