96a6d0f34586b7807be910be20875c4b3b943d4a
Mohamed Hussein <mohamedmhussein@protonmail.com>
2024-12-17 22:30:09 -0500

    Adds BST

---
+++ BST.js
@@ -0,0 +1,42 @@
+class Node{
+    constructor(value){
+        this.value = value;
+        this.left = null;
+        this.right = null
+    }
+}
+
+class BST{
+    constructor(){
+        this.root = null
+    }
+
+    insert(value){
+        var newNode = new Node(value);
+        if(this.root === null){
+            this.root = newNode;
+            return this;
+        }
+        var current = this.root;
+        while(true){
+            if(value === current.value) return undefined;
+            
+            //left
+            if(value < current.value){
+                if(current.left === null){
+                    current.left = newNode;
+                    return this;
+                }
+                current = current.left;
+            }
+            //right
+            else {
+                if(current.right === null){
+                    current.right = newNode;
+                    return this;
+                } 
+                current = current.right;
+            }
+        }
+    }
+}
